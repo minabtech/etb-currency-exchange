496a934c4abcaf5cf54a7c71581a86c19829f70e
Mikiyas Tibebu <mikitebe123@gmail.com>
2022-07-13 18:07:12 +0300

    update url and added additional fields

--- index.js
+++ index.js
@@ -1,35 +1,39 @@
-const cheerio = require('cheerio')
-const cheerioTableparser = require('cheerio-tableparser');
-const request = require('request');
+const cheerio = require("cheerio");
+const cheerioTableparser = require("cheerio-tableparser");
+const request = require("request");
 
-const getRate = () => new Promise((resolve, reject) => {
-    request({
-        uri: "https://dashenbanksc.com/cashexchangerate/rate.php",
-    }, (error, response, body) => {
+const getRate = () =>
+  new Promise((resolve, reject) => {
+    request(
+      {
+        uri: "https://dashenbanksc.com/daily-exchange-rates",
+      },
+      (error, response, body) => {
         if (body) {
-            let $ = cheerio.load(body);
+          let $ = cheerio.load(body);
 
-            cheerioTableparser($);
+          cheerioTableparser($);
 
-            let data = $("table").parsetable();
+          let data = $("table").parsetable();
 
-            if(!data || data.length == 0) return resolve([]);
+          if (!data || data.length == 0) return resolve([]);
 
-            let rate = [];
-            for (let i = 1, l = data[0].length; i < l; i++) {
-                rate.push({
-                    currency: data[0][i].replace(/\n &#xA0;/g, ''),
-                    buying: data[1][i].replace(/\n &#xA0;/g, ''),
-                    selling: data[2][i].replace(/\n &#xA0;/g, ''),
-                })
-            }
-            return resolve(rate);
-        } else return reject(error)
-    });
-});
+          let rate = [];
+          for (let i = 1; i < 18; i++) {
+            rate.push({
+              currency: data[1][i].replace(/&nbsp;\s*/g, ""),
+              buying: data[2][i].replace(/&nbsp;\s*/g, ""),
+              selling: data[3][i].replace(/&nbsp;\s*/g, ""),
+              "transaction buying": data[4][i].replace(/&nbsp;\s*/g, ""),
+              "transaction selling": data[5][i].replace(/&nbsp;\s*/g, ""),
+            });
+          }
+          return resolve(rate);
+        } else return reject(error);
+      }
+    );
+  });
 
 module.exports = {
-    getRate: getRate
-}
-
-
+  getRate: getRate,
+};
